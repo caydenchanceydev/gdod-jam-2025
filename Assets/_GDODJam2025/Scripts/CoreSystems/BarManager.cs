db3838ba60d6990f6e6231d1e770c9f49fa389dc
Cayden Chancey <109098263+caydenchanceyDH@users.noreply.github.com>
2025-03-04 04:30:42 -0600

    added tri bar interaction

---
+++ Assets/_GDODJam2025/Scripts/CoreSystems/BarManager.cs
@@ -0,0 +1,123 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+using Sirenix.OdinInspector;
+
+public class BarManager : MonoBehaviour
+{
+    [Title("UI")]
+    public Slider drinkSlider;
+    public Slider bladderSlider;
+    public Slider anxietySlider;
+    
+    [Title("Start Values")]
+    [Range(0, 1)] 
+    public float drinkStart;
+    [Range(0, 1)] 
+    public float bladderStart;
+    [Range(0, 1)] 
+    public float anxietyStart;
+    
+    [Title("Current Values")]
+    [Range(0, 1), ReadOnly] 
+    public float drink;
+    [Range(0, 1), ReadOnly] 
+    public float bladder;
+    [Range(0, 1), ReadOnly] 
+    public float anxiety;
+    
+    [Title("Tick Settings")]
+    public float tickRate;
+    
+    [Title("Passive Drain Rates")]
+    public float drinkDrainRate;  // Passive Drink decrease per tick
+
+    [Title("Threshold Ranges")] 
+    [Range(0, 1)]
+    public float drinkMinBladder;
+    [Range(0, 1)]
+    public float drinkMaxBladder;
+    [Range(0, 1)] 
+    public float drinkMinAnxiety;
+    [Range(0, 1)]
+    public float drinkMaxAnxiety;
+    [Range(0, 1)] 
+    public float bladderMinAnxiety;
+    [Range(0, 1)]
+    public float bladderMaxAnxiety;
+    
+    [Title("Influence Multipliers")]
+    public float drinkToBladder;
+    public float drinkToAnxiety;
+    public float bladderToAnxiety;
+
+    private void Start()
+    {
+        SetStartValues();
+        
+        InvokeRepeating(nameof(UpdateStatus), tickRate, tickRate);
+    }
+
+    private void SetStartValues()
+    {
+        drink = drinkStart;
+        bladder = bladderStart;
+        anxiety = anxietyStart;
+        
+        drinkSlider.value = drink;
+        bladderSlider.value = bladder;
+        anxietySlider.value = anxiety;
+    }
+
+    private void UpdateStatus()
+    {
+        // Passive drink drain
+        drink -= drinkDrainRate;
+        
+        // Drink influences bladder
+        if (drink >= drinkMinBladder && drink <= drinkMaxBladder)
+        {
+            bladder += drink * drinkToBladder;
+        }
+
+        // Drink influences anxiety
+        if (drink >= drinkMinAnxiety && drink <= drinkMaxAnxiety)
+        {
+            anxiety += drink * drinkToAnxiety;
+        }
+
+        // Bladder influences anxiety
+        if (bladder >= bladderMinAnxiety && bladder <= bladderMaxAnxiety)
+        {
+            anxiety += bladder * bladderToAnxiety;
+        }
+        
+        drink = Mathf.Clamp01(drink);
+        bladder = Mathf.Clamp01(bladder);
+        anxiety = Mathf.Clamp01(anxiety);
+        
+        drinkSlider.value = drink;
+        bladderSlider.value = bladder;
+        anxietySlider.value = anxiety;
+    }
+    
+    [Title("Debug Interaction")]
+    
+    [Button("Modify Drink")]
+    public void ModifyDrink(float amount)
+    {
+        drink = Mathf.Clamp01(drink + amount);
+    }
+
+    [Button("Modify Anxiety")]
+    public void ModifyAnxiety(float amount)
+    {
+        anxiety = Mathf.Clamp01(anxiety + amount);
+    }
+    
+    [Button("Modify Bladder")]
+    public void ModifyBladder(float amount)
+    {
+        bladder = Mathf.Clamp01(bladder + amount);
+    }
+}
